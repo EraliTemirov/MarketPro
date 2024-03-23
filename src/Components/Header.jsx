71b50eaeea54221e8dc69c81948f54903e61b9db
Temirov Erali <eralitemirov129@gmail.com>
2024-03-23 12:22:18 +0500

    second

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -25,7 +25,7 @@ function Header() {
 </form>
 
      <button className="flex items-center">
-     <svg className="h-8 w-8 text-black-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  >  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />  <circle cx="12" cy="7" r="4" /> </svg>
+     <svg className="h-8 w-8 text-black-600"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  >  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />  <circle cx="12" cy="7" r="4" /> </svg>
      <span>Mening kabinetim</span>
      </button>
      </div>
